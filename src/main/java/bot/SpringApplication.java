42a8a257a338ec12deef25f9da035f46de0c3e05
Mikhail Soloschenko <mmmsss.88@mail.ru>
2021-12-02 09:50:02 +0300

    changed vaadin version

--- src/main/java/bot/SpringApplication.java
+++ src/main/java/bot/SpringApplication.java
@@ -1,12 +1,17 @@
 package bot;
 
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.vaadin.artur.helpers.LaunchUtil;
 
 @SpringBootApplication
+@Theme(value = "gridpaging")
+@PWA(name = "Grid Paging", shortName = "Grid Paging", offlineResources = {"images/logo.png"})
 public class SpringApplication {
 
     public static void main(String[] args) {
-        org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+        LaunchUtil.launchBrowserInDevelopmentMode(org.springframework.boot.SpringApplication.run(SpringApplication.class, args));
     }
 
 }
---
+++ src/main/java/bot/ui/BotUserEndPoint.java
@@ -0,0 +1,9 @@
+package bot.ui;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.server.connect.Endpoint;
+
+@Endpoint
+@AnonymousAllowed
+public class BotUserEndPoint {
+}
